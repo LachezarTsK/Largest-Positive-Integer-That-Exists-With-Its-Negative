b511d256c07864bd8655cf318850b835b709bd9b
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-02 18:12:08 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,19 @@
+
+function findMaxK(input: number[]): number {
+    const RANGE_OF_VALUES = [-1000, 1000];
+    const OFFSET = Math.abs(RANGE_OF_VALUES[0]);
+    const NOT_FOUND = -1;
+
+    const foundValues: boolean[] = new Array(RANGE_OF_VALUES[1] + OFFSET + 1).fill(false);
+    for (let value of input) {
+        foundValues[value + OFFSET] = true;
+    }
+
+    for (let value = RANGE_OF_VALUES[1] + OFFSET; value >= OFFSET; --value) {
+        if (foundValues[value] && foundValues[OFFSET - (value - OFFSET)]) {
+            return value - OFFSET;
+        }
+    }
+
+    return NOT_FOUND;
+};
